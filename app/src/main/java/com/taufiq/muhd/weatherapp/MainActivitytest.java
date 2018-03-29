1d1a045d0fb51d6987f4495bbe5d0bceb3394a09
topek <heckcrack3r@gmail.com>
2018-03-29 15:30:38 +0800

    volley

--- app/src/main/java/com/taufiq/muhd/weatherapp/MainActivitytest.java
+++ app/src/main/java/com/taufiq/muhd/weatherapp/MainActivitytest.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +16,6 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -33,7 +32,7 @@ import java.net.ProtocolException;
 import java.net.URL;
 import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivitytest extends AppCompatActivity {
 
     private TextView tvLocation, tvTemperature, tvHumidity, tvWindSpeed, tvCloudiness;
     private Button btnRefresh;
@@ -57,8 +56,7 @@ public class MainActivity extends AppCompatActivity {
     btnRefresh.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            //new WeatherDataRetrival().execute();
-            requestViaVolley();
+            new WeatherDataRetrival().execute();
         }
     });
     }
@@ -78,119 +76,6 @@ public class MainActivity extends AppCompatActivity {
         });
         queue.add(stringRequest);
     }
-
-    private void parse(String result){
-        JSONObject weatherJSON = null;
-        if (result != null)
-            try {
-                weatherJSON = new JSONObject(result);
-                tvLocation.setText(weatherJSON.getString("name") + "," + weatherJSON.getJSONObject("sys").getString("country"));
-                tvWindSpeed.setText(String.valueOf(weatherJSON.getJSONObject("wind").getDouble("speed")) + "mps");
-                tvCloudiness.setText(String.valueOf(weatherJSON.getJSONObject("clouds").getInt("all")) + "%");
-                final JSONObject mainJSON = weatherJSON.getJSONObject("main");
-
-                Double temp = mainJSON.getDouble("temp");
-                DecimalFormat formatD = new DecimalFormat("#");
-                tvTemperature.setText(formatD.format(temp-273));
-                tvHumidity.setText(String.valueOf(mainJSON.getInt("humidity")) + "%");
-
-                final JSONArray weatherJSONArray = weatherJSON.getJSONArray("weather");
-                if (weatherJSONArray.length()>0){
-                    int code = weatherJSONArray.getJSONObject(0).getInt("id");
-                    ivIcon.setImageResource(getIcon(code));
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-    }
-    private int getIcon(int code) {
-        switch (code) {
-            case 200:
-            case 201:
-            case 202:
-            case 210:
-            case 211:
-            case 212:
-            case 221:
-            case 230:
-            case 231:
-            case 232:
-                return R.drawable.ic_thunderstorm_large;
-
-            case 300:
-            case 301:
-            case 302:
-            case 310:
-            case 311:
-            case 312:
-            case 313:
-            case 314:
-            case 321:
-                return R.drawable.ic_drizzle_large;
-
-            case 500:
-            case 501:
-            case 502:
-            case 503:
-            case 504:
-            case 511:
-            case 520:
-            case 521:
-            case 522:
-            case 531:
-                return R.drawable.ic_rain_large;
-
-            case 600:
-            case 601:
-            case 602:
-            case 611:
-            case 612:
-            case 615:
-            case 616:
-            case 620:
-            case 621:
-            case 622:
-                return R.drawable.ic_snow_large;
-
-            case 800:
-                return R.drawable.ic_day_clear_large;
-
-            case 801:
-                return R.drawable.ic_day_few_clouds_large;
-
-            case 802:
-                return R.drawable.ic_scattered_clouds_large;
-
-            case 803:
-            case 804:
-                return R.drawable.ic_broken_clouds_large;
-
-            case 701:
-            case 711:
-            case 721:
-            case 731:
-            case 741:
-            case 751:
-            case 761:
-            case 762:
-                return R.drawable.ic_fog_large;
-
-            case 781:
-            case 900:
-                return R.drawable.ic_tornado_large;
-
-            case 905:
-                return R.drawable.ic_windy_large;
-
-            case 906:
-                return R.drawable.ic_hail_large;
-
-            default:
-                return R.drawable.ic_day_clear_large;
-
-        }
-    }
-
 /*volley testing close*/
     private class WeatherDataRetrival extends AsyncTask<Void, Void, String> {
         @Override
@@ -201,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... voids) {
 
-            NetworkInfo networkInfo = ((ConnectivityManager)MainActivity.this.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+            NetworkInfo networkInfo = ((ConnectivityManager)MainActivitytest.this.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
             if (networkInfo != null && networkInfo.isConnected()){
                 //network-connected
                 URL url = null;
@@ -244,9 +129,119 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-            parse(result);
+            JSONObject weatherJSON = null;
+            if (result != null)
+            try {
+                weatherJSON = new JSONObject(result);
+                tvLocation.setText(weatherJSON.getString("name") + "," + weatherJSON.getJSONObject("sys").getString("country"));
+                tvWindSpeed.setText(String.valueOf(weatherJSON.getJSONObject("wind").getDouble("speed")) + "mps");
+                tvCloudiness.setText(String.valueOf(weatherJSON.getJSONObject("clouds").getInt("all")) + "%");
+                final JSONObject mainJSON = weatherJSON.getJSONObject("main");
+
+                Double temp = mainJSON.getDouble("temp");
+                DecimalFormat formatD = new DecimalFormat("#");
+                tvTemperature.setText(formatD.format(temp-273));
+                tvHumidity.setText(String.valueOf(mainJSON.getInt("humidity")) + "%");
+
+                final JSONArray weatherJSONArray = weatherJSON.getJSONArray("weather");
+                if (weatherJSONArray.length()>0){
+                    int code = weatherJSONArray.getJSONObject(0).getInt("id");
+                    ivIcon.setImageResource(getIcon(code));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
+
+        private int getIcon(int code) {
+            switch (code)
+            {
+                case 200:
+                case 201:
+                case 202:
+                case 210:
+                case 211:
+                case 212:
+                case 221:
+                case 230:
+                case 231:
+                case 232:
+                    return R.drawable.ic_thunderstorm_large;
+
+                case 300:
+                case 301:
+                case 302:
+                case 310:
+                case 311:
+                case 312:
+                case 313:
+                case 314:
+                case 321:
+                    return R.drawable.ic_drizzle_large;
+
+                case 500:
+                case 501:
+                case 502:
+                case 503:
+                case 504:
+                case 511:
+                case 520:
+                case 521:
+                case 522:
+                case 531:
+                    return R.drawable.ic_rain_large;
+
+                case 600:
+                case 601:
+                case 602:
+                case 611:
+                case 612:
+                case 615:
+                case 616:
+                case 620:
+                case 621:
+                case 622:
+                    return R.drawable.ic_snow_large;
+
+                case 800:
+                    return R.drawable.ic_day_clear_large;
+
+                case 801:
+                    return R.drawable.ic_day_few_clouds_large;
+
+                case 802:
+                    return R.drawable.ic_scattered_clouds_large;
+
+                case 803:
+                case 804:
+                    return R.drawable.ic_broken_clouds_large;
+
+                case 701:
+                case 711:
+                case 721:
+                case 731:
+                case 741:
+                case 751:
+                case 761:
+                case 762:
+                    return R.drawable.ic_fog_large;
+
+                case 781:
+                case 900:
+                    return R.drawable.ic_tornado_large;
+
+                case 905:
+                    return R.drawable.ic_windy_large;
+
+                case 906:
+                    return R.drawable.ic_hail_large;
+
+                default: return R.drawable.ic_day_clear_large;
+
+            }
+
+        }
+
     }
 }
-
 
